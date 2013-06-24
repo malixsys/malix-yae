f0c840b50555c5e5edb7f7337843fd647492b266
Martin Alix <malixsys@Martins-MacBook-Air.local>
2013-06-24 13:11:52 -0400

    switched to animals

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -9,57 +9,67 @@ function findById(a, id) {
 
 var app = angular.module('sample', ['ui.compat'])
   .config(
-    [        '$stateProvider', '$routeProvider', '$urlRouterProvider',
-      function ($stateProvider, $routeProvider, $urlRouterProvider) {
+    [          '$stateProvider', '$routeProvider', '$urlRouterProvider',
+      function ($stateProvider,   $routeProvider,   $urlRouterProvider) {
         $urlRouterProvider
-          .when('/c?id', '/contacts/:id')
+          .when('/c?id', '/animals/:id')
           .otherwise('/');
 
         $routeProvider
           .when('/user/:id', {
-            redirectTo: '/contacts/:id',
+            redirectTo: '/animals/:id',
           })
           .when('/', {
             templateUrl: 'views/main.html',
+            controller: [        '$scope', '$state',
+                    function ($scope, $state) {
+                        $scope.username = "guest";
+                    }]
           });
 
         $stateProvider
-          .state('contacts', {
-            url: '/contacts',
+          .state('animals', {
+            url: '/animals',
             abstract: true,
-            templateUrl: 'views/contacts.html',
+            templateUrl: 'views/animals.html',
             controller: [        '$scope', '$state',
               function ($scope, $state) {
-                $scope.contacts = [
+                $scope.animals = [
                   {
                     id: 1,
-                    name: "Alice",
+                    name: "Nellie",
                     items: [
                       {
                         id: 'a',
-                        type: 'phone number',
-                        value: '555-1234-1234',
+                        type: 'medal',
+                        value: '55512341234',
                       },
                       {
                         id: 'b',
-                        type: 'email',
-                        value: 'alice@mailinator.com',
-                      }
+                        type: 'race',
+                        value: 'poodle',
+                      },
+                      {
+                        id: 'c',
+                        type: 'species',
+                        value: 'dog',
+                      },
+
                     ],
                   },
                   {
                     id: 42,
-                    name: "Bob",
+                    name: "Rex",
                     items: [
                       {
                         id: 'a',
-                        type: 'blog',
-                        value: 'http://bob.blogger.com',
+                        type: 'species',
+                        value: 'cat',
                       },
                       {
                         id: 'b',
-                        type: 'fax',
-                        value: '555-999-9999',
+                        type: 'age',
+                        value: '10',
                       }
                     ],
                   },
@@ -69,49 +79,49 @@ var app = angular.module('sample', ['ui.compat'])
                     items: [
                       {
                         id: 'a',
-                        type: 'full name',
-                        value: 'Eve Adamsdottir',
+                        type: 'species',
+                        value: 'snake',
                       }
                     ],
                   }
                 ];
 
                 $scope.goToRandom = function () {
-                  var contacts = $scope.contacts, id;
+                  var animals = $scope.animals, id;
                   do {
-                    id = contacts[Math.floor(contacts.length * Math.random())].id;
-                  } while (id == $state.params.contactId);
-                  $state.transitionTo('contacts.detail', { contactId: id });
+                    id = animals[Math.floor(animals.length * Math.random())].id;
+                  } while (id == $state.params.animalId);
+                  $state.transitionTo('animals.detail', { animalId: id });
                 };
               }],
           })
-          .state('contacts.list', {
-            // parent: 'contacts',
+          .state('animals.list', {
+            // parent: 'animals',
             url: '',
-            templateUrl: 'views/contacts.list.html',
+            templateUrl: 'views/animals.list.html',
           })
-          .state('contacts.detail', {
-            // parent: 'contacts',
-            url: '/{contactId}',
+          .state('animals.detail', {
+            // parent: 'animals',
+            url: '/{animalId}',
             resolve: {
               something: [        '$timeout', '$stateParams',
                 function ($timeout, $stateParams) {
                   return $timeout(function () {
-                    return "Asynchronously resolved data (" + $stateParams.contactId + ")"
+                    return "Asynchronously resolved data (" + $stateParams.animalId + ")"
                   }, 10);
                 }],
             },
             views: {
               '': {
-                templateUrl: 'views/contacts.detail.html',
+                templateUrl: 'views/animals.detail.html',
                 controller: [        '$scope', '$stateParams', 'something',
                   function ($scope, $stateParams, something) {
                     $scope.something = something;
-                    $scope.contact = findById($scope.contacts, $stateParams.contactId);
+                    $scope.animal = findById($scope.animals, $stateParams.animalId);
                   }],
               },
               'hint@': {
-                template: 'This is contacts.detail populating the view "hint@"',
+                template: 'This is animals.detail populating the view "hint@"',
               },
               'menu': {
                 templateProvider: [ '$stateParams',
@@ -119,22 +129,22 @@ var app = angular.module('sample', ['ui.compat'])
                     // This is just to demonstrate that $stateParams injection works for templateProvider
                     // $stateParams are the parameters for the new state we're transitioning to, even
                     // though the global '$stateParams' has not been updated yet.
-                    return '<hr><small class="muted">Contact ID: ' + $stateParams.contactId + '</small>';
+                    return '<hr><small class="muted">animal ID: ' + $stateParams.animalId + '</small>';
                   }],
               },
             },
           })
-          .state('contacts.detail.item', {
-            // parent: 'contacts.detail',
+          .state('animals.detail.item', {
+            // parent: 'animals.detail',
             url: '/item/:itemId',
             views: {
               '': {
-                templateUrl: 'views/contacts.detail.item.html',
+                templateUrl: 'views/animals.detail.item.html',
                 controller: [        '$scope', '$stateParams', '$state',
                   function ($scope, $stateParams, $state) {
-                    $scope.item = findById($scope.contact.items, $stateParams.itemId);
+                    $scope.item = findById($scope.animal.items, $stateParams.itemId);
                     $scope.edit = function () {
-                      $state.transitionTo('contacts.detail.item.edit', $stateParams);
+                      $state.transitionTo('animals.detail.item.edit', $stateParams);
                     };
                   }],
               },
@@ -143,15 +153,15 @@ var app = angular.module('sample', ['ui.compat'])
               },
             },
           })
-          .state('contacts.detail.item.edit', {
+          .state('animals.detail.item.edit', {
             views: {
-              '@contacts.detail': {
-                templateUrl: 'views/contacts.detail.item.edit.html',
+              '@animals.detail': {
+                templateUrl: 'views/animals.detail.item.edit.html',
                 controller: [        '$scope', '$stateParams', '$state',
                   function ($scope, $stateParams, $state) {
-                    $scope.item = findById($scope.contact.items, $stateParams.itemId);
+                    $scope.item = findById($scope.animal.items, $stateParams.itemId);
                     $scope.done = function () {
-                      $state.transitionTo('contacts.detail.item', $stateParams);
+                      $state.transitionTo('animals.detail.item', $stateParams);
                     };
                   }],
               },
